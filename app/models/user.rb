0f456e3e52ce2107f1fcbd1543f57b32cb19cf6a
Nikita Silivonchik <lstpsche@gmail.com>
2019-10-05 03:34:20 +0300

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,49 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  serialize :context, ::Serializers::HashSerializer
+  store_accessor :context, :last_message, :replace_last_message, :return_to
+  has_many :schedule_users
+  has_many :schedules, through: :schedule_users
+
+  def method_missing(method_name, *args, &block)
+    nil
+  end
+
+  # message.message.message_id — получить id сообщения, от которого пришёл callback
+  # TODO:
+  # 1) сохранять _message.message_ в user.context['tapped_message'] (будет доступно по user.tapped_message)
+  # 2) при эдите сообщения, эдитить не последнее, а tapped_message
+
+  def empty_context
+    {
+      'last_message' => {
+        'result' => {
+          'message_id' => nil
+        }
+      },
+      'replace_last_message' => false,
+      'return_to' => nil
+    }
+  end
+
+  def context
+    super.empty? ? empty_context : super
+  end
+
+  def last_message
+    context['last_message']
+  end
+
+  def last_message_id
+    last_message['result']['message_id']
+  end
+
+  def replace_last_message?
+    context['replace_last_message']
+  end
+
+  def return_to
+    context['return_to']
+  end
 end
