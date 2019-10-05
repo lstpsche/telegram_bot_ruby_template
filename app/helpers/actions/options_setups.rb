25c92fc0353027f0dbfc7c10fafcbdb5fadcc485
Nikita Silivonchik <lstpsche@gmail.com>
2019-10-05 03:37:11 +0300

    Update actions helpers

---
+++ app/helpers/actions/options_setups.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Helpers
+  module Actions
+    module OptionsSetups
+      def setup_example_option_1
+        setup('example_option_1')
+      end
+
+      # TEMPLATE_TODO: add methods with the following syntax: "setup_#{option_name_in_snake_case}"
+    end
+  end
+end
--- app/helpers/actions/users_helper.rb
+++ app/helpers/actions/users_helper.rb
@@ -3,15 +3,37 @@
 module Helpers
   module Actions
     module UsersHelper
-      def send_option_message(option_name, chat_id, markup = nil)
-        message_text = I18n.t("actions.users.options.#{option_name}")
-        talker.send_message(text: message_text, chat_id: chat_id, markup: markup)
+      def get_response
+        # THIS RESPONSE CAN BE FROM ANOTHER PERSON
+        # SHOULD TEST IT AND MAYBE ADD CHECKER, IF RESPONSE IS FROM NEEDED USER
+        @response = get_message
+
+        case @response
+        when Telegram::Bot::Types::Message
+          return @response.text
+        when Telegram::Bot::Types::CallbackQuery
+          return @response.data
+        end
+      end
+
+      def send_option_message(option_name, user, markup = nil)
+        message_text = I18n.t("actions.users.options.#{option_name}.text")
+        send_or_edit_message(
+          message_id: user.last_message_id, text: message_text,
+          markup: markup
+        )
       end
 
       def setup_successfull
-        talker.send_message(
+        send_message(
           text: I18n.t('actions.users.preferences.setup_successful'),
-          chat_id: chat_id,
+          markup: 'remove'
+        )
+      end
+
+      def show_successfully_setup
+        send_message(
+          text: I18n.t('actions.users.options.setup_successful'),
           markup: 'remove'
         )
       end
@@ -21,20 +43,21 @@ module Helpers
 
         markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: options_kb)
 
-        talker.send_message(
-          text: I18n.t('actions.users.preferences.show_options'),
-          chat_id: chat_id,
+        send_or_edit_message(
+          message_id: user.last_message_id, text: I18n.t('actions.users.preferences.show_options'),
           markup: markup
         )
       end
 
-      def options_menu_inline_buttons(context)
+      def options_menu_inline_buttons(context: nil)
         options_kb = []
 
-        Constants.options.each do |option_name|
-          text = option_name.split('_').join(' ').capitalize
-          callback = "preferences-#{option_name}" + ((context.nil? && context.strip.empty?) ? "-#{context}" : nil)
-          options_kb << Telegram::Bot::Types::InlineKeyboardButton.new(text: text, callback_data: callback)
+        Constants.preferences_options.each do |option|
+          text = option[:button]
+          callback = "preferences-show_#{option[:name]}" + ((context.nil? || context.strip.empty?) ? '' : "-#{context}")
+          options_kb << Telegram::Bot::Types::InlineKeyboardButton.new(
+            text: text, callback_data: callback
+          )
         end
 
         options_kb
